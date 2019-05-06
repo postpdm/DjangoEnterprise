a8f397bf4ce21db42ae4877872676e611a0447f5
yn-coder <yn.coder@gmail.com>
2019-05-06 10:51:27 +0300

    Create __init__.py

---
+++ djangoenterprise/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+# :repository: https://github.com/postpdm/DjangoEnterprise
+
+# from models import *
+
+__version__ = '0.0.0'
+__author__ = 'YN.Coder'
+
+__all__ = [
+]
