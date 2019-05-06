bb89c8db6ed5d89562ac2622af07fb22cd941c70
yn-coder <yn.coder@gmail.com>
2019-05-06 09:55:06 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='earthscipy',
+      version='0.0.0',
+      description='Earth science lib',
+      url='https://github.com/postpdm/DjangoEnterprise',
+      author='YN.Coder',
+      author_email='yn.coder@gmail.com',
+      license='-',
+      packages=['djangoenterprise'],
+      zip_safe=False)
